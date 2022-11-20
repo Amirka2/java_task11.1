0da63810e81721108b19d483cd8d4e7c4e05bce0
Amir <70907990+Amirka2@users.noreply.github.com>
2022-11-20 23:55:42 +0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,21 @@ public class Main {
     static int count = 0;
 
     static class LuckyThread extends Thread {
+        String name;
+        public LuckyThread(String name) {
+            this.name = name;
+        }
         @Override
         public void run() {
             calc();
         }
-        private static synchronized void calc() {
+        private synchronized void calc() {
             while (x < 999999) {
                 x++;
                 if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
                         % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                    System.out.println(x);
+                    System.out.print(x);
+                    System.out.println(name);
                     count++;
                 }
             }
@@ -24,9 +29,9 @@ public class Main {
 
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new LuckyThread();
-        Thread t2 = new LuckyThread();
-        Thread t3 = new LuckyThread();
+        Thread t1 = new LuckyThread("t1");
+        Thread t2 = new LuckyThread("t2");
+        Thread t3 = new LuckyThread("t3");
         t1.start();
         t2.start();
         t3.start();
